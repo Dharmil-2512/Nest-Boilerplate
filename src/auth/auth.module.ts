659a116e1ebf26ef159c9a378acf71abfa87111b
dharmil-vc <dharmil.shah@viitor.cloud>
2024-01-23 11:28:21 +0530

    Fixed mongooseModule in authModule

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,6 +1,5 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
-import { Schema } from 'mongoose';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { EmailVerify, emailVerifySchema } from './schemas/email-verify.schema';
@@ -11,15 +10,9 @@ import {
 
 @Module({
   imports: [
-    MongooseModule.forFeatureAsync([
-      {
-        name: EmailVerify.name,
-        useFactory: (): Schema<EmailVerify> => emailVerifySchema,
-      },
-      {
-        name: ForgotPassword.name,
-        useFactory: (): Schema<ForgotPassword> => forgotPasswordSchema,
-      },
+    MongooseModule.forFeature([
+      { name: EmailVerify.name, schema: emailVerifySchema },
+      { name: ForgotPassword.name, schema: forgotPasswordSchema },
     ]),
   ],
   controllers: [AuthController],
